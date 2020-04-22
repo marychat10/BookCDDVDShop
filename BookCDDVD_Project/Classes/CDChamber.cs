7134a4c53e7a772cb48dfae6186868430934c948
RoseKeenan <rosemkeenan@gmail.com>
2020-04-22 15:46:38 -0400

    Added other CDs

---
+++ BookCDDVD_Project/Classes/CDChamber.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookCDDVD_Project.Classes
+{
+    [Serializable()]
+
+    class CDChamber : CDClassical
+    {
+        private string[] hiddenInstrumentList;
+
+        public CDChamber()
+        {
+        }
+
+        // Parameterized Constructor
+        public CDChamber(int UPC, decimal price, string title, int quantity, string label, string[] artists, string[] instrumentList) : base(UPC, price, title, quantity, label, artists)
+        {
+            hiddenInstrumentList = instrumentList;
+        }  // end Employee Parameterized Constructor
+
+
+        // Accessor/mutator for Book ISBN
+        public string[] CDInstrumentList
+        {
+            get
+            {
+                return hiddenInstrumentList;
+            }  // end get
+            set   // (string value)
+            {
+                hiddenInstrumentList = value;
+            }  // end get
+        }  // end Property
+
+
+        public override void Save(frmBookCDDVD f)
+        {
+            base.Save(f);
+            hiddenInstrumentList = (f.txtInstruments.Text).Split(',');
+        } // end Save
+
+
+        // Display data in object on form
+        public override void Display(frmBookCDDVD f)
+        {
+            base.Display(f);
+            f.txtInstruments.Text = string.Join(",", hiddenInstrumentList);
+        }  // end Display
+
+
+        // This toString function overrides the Object toString
+        //     function.  The base refers to Object because this class
+        //     inherits Object by default.
+        public override string ToString()
+        {
+            string s = base.ToString() + "\n";
+            s += "CD Info: " + hiddenInstrumentList.ToString();
+            return s;
+        }  // end ToString
+
+    }  // end Book class
+}  // end namespace  
+
+
+
+
+
